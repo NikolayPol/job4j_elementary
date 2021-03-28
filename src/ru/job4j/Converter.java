9ef59ae2d78fcfff3e6117952d45eb04987d3eda
NikolayPolegaev <N5445@yandex.ru>
2021-03-28 13:52:05 +0300

    5. Конвертер валюты [#504873]

--- src/ru/job4j/Converter.java
+++ src/ru/job4j/Converter.java
@@ -1,15 +1,12 @@
 package ru.job4j;
 
-public class Converter
-{
+public class Converter {
     public static float rubleToEuro(int value) {
-        float rsl = (float) value / 70;
-        return rsl;
+        return value / 70.0f;
     }
 
     public static float rubleToDollar(int value) {
-        float rsl = (float) value / 60; /* формула перевода рублей в доллары. */
-        return rsl;
+        return value / 60.0f;
     }
 
     public static void main(String[] args) {
