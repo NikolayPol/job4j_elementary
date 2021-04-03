616b495265c43fd7faf851d269d47a437e73a340
NikolayPolegaev <N5445@yandex.ru>
2021-04-03 11:30:50 +0300

    5.6. Простые числа [#504933]

--- test/ru/job4j/array/PrimeNumber.java
+++ test/ru/job4j/array/PrimeNumber.java
@@ -1,9 +1,11 @@
-package ru.job4j.loop;
+package ru.job4j.array;
+
+import ru.job4j.loop.CheckPrimeNumber;
 
 public class PrimeNumber {
     public static int calc(int finish) {
         int count = 0;
-        for (int number = 1; number < finish; number++) {
+        for (int number = 2; number <= finish; number++) {
             if (CheckPrimeNumber.check(number)) {
                 count++;
             }
--- test/ru/job4j/loop/PrimeNumberTest.java
+++ test/ru/job4j/loop/PrimeNumberTest.java
@@ -2,8 +2,7 @@ package ru.job4j.loop;
 
 import org.junit.Assert;
 import org.junit.Test;
-
-import static org.junit.Assert.*;
+import ru.job4j.array.PrimeNumber;
 
 public class PrimeNumberTest {
 
